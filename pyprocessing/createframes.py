1696f156ac361cb667781a586d217afd42749d28
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2015-02-23 16:01:29 +0000

    refactored file check assertion, so it raises an error with a useful message

--- pyprocessing/createframes.py
+++ pyprocessing/createframes.py
@@ -7,6 +7,10 @@ class ReadData:
 
     def __init__(self, filename):
 
-        assert os.path.exists(filename)
+        try:
+            assert os.path.exists(filename)
+        except AssertionError as e:
+            e.args += (filename, 'does not exist')
+            raise
 
         self.filename = filename
