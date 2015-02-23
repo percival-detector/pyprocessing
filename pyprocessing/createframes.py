3b4fb3aff07a6caf939dddd3848ac89f5cc207fe
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2015-02-23 16:29:45 +0000

    added check for h5//hdf5 file name extension, as for now we are only going to read hdf5 files for data

--- pyprocessing/createframes.py
+++ pyprocessing/createframes.py
@@ -5,6 +5,9 @@ import os
 
 def ReadData(filename):
 
+    if not filename.endswith(('.h5','.hdf5')):
+        raise IOError('filename does not end with hdf5 format extension')
+
     try:
         assert os.path.exists(filename)
     except AssertionError as e:
--- pyprocessing/test_createframes.py
+++ pyprocessing/test_createframes.py
@@ -16,7 +16,7 @@ class ReadDataTest(unittest.TestCase):
         pass
 
     @patch('createframes.os.path.exists')
-    def test_given_filename_in_argument_when_class_invoked_then_check_file_existence(self, mock_exists):
+    def test_given_filename_in_argument_when_method_invoked_then_check_file_existence(self, mock_exists):
 
         filename = 'some_file.hdf5'
 
@@ -25,15 +25,37 @@ class ReadDataTest(unittest.TestCase):
         mock_exists.assert_called_once_with(filename)
 
     @patch('createframes.os.path.exists',return_value=False)
-    def test_given_filename_in_argument_when_class_invoked_then_assertion_error_if_file_nonexistent(self, mock_exists):
+    def test_given_filename_in_argument_when_method_invoked_then_assertion_error_if_file_nonexistent(self, mock_exists):
 
         filename = 'some_file.hdf5'
 
         with self.assertRaises(AssertionError):
             createframes.ReadData(filename)
 
-    def test_given_real_hdf5_file_when_class_invoked_then_hdf5_file_type_recognised(self):
-        pass
+    @patch('createframes.os.path.exists',return_value=True)
+    def test_given_real_hdf5_file_when_class_invoked_then_hdf5_file_type_recognised(self, mock_exists):
+        
+        jpg_filename = 'some.jpg'
+        h5_filename = 'some.h5'
+        hdf5_filename = 'some.hdf5'
+        not_h5_filename = 'some.noth5'
+
+        with self.assertRaises(IOError):
+            createframes.ReadData(jpg_filename)
+
+        try:
+            createframes.ReadData(h5_filename)
+        except IOError:
+            self.fail('createframes.ReadData IOerror raised')
+
+        try:
+            createframes.ReadData(hdf5_filename)
+        except IOError:
+            self.fail('createframes.ReadData IOerror raised')
+
+        with self.assertRaises(IOError):
+            createframes.ReadData(not_h5_filename)
+
 
 
 if __name__ == '__main__':
