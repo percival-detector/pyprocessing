6b6860ec3eb8ec1c0e27be38f0c6a0ae219f72a3
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2015-02-23 15:29:40 +0000

    class called ReadData that checks file type exists, along with unittests for this

--- pyprocessing/createframes.py
+++ pyprocessing/createframes.py
@@ -1,3 +1,12 @@
 # createframe.py
 #
 # Author: Arvinder Palaha
+import os
+
+class ReadData:
+
+    def __init__(self, filename):
+
+        assert os.path.exists(filename)
+
+        self.filename = filename
--- pyprocessing/test_createframes.py
+++ pyprocessing/test_createframes.py
@@ -7,13 +7,31 @@ from mock import patch, ANY
 import createframes
 
 
-class CreateFramesTest(unittest.TestCase):
+class ReadDataTest(unittest.TestCase):
 
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
+
+    @patch('createframes.os.path.exists')
+    def test_given_filename_in_argument_when_class_invoked_then_check_file_existence(self, mock_exists):
+
+        filename = 'some_file.hdf5'
+
+        createframes.ReadData(filename)
+
+        mock_exists.assert_called_once_with(filename)
+
+    @patch('createframes.os.path.exists',return_value=False)
+    def test_given_filename_in_argument_when_class_invoked_then_assertion_error_if_file_nonexistent(self, mock_exists):
+
+        filename = 'some_file.hdf5'
+
+        with self.assertRaises(AssertionError):
+            createframes.ReadData(filename)
+
 
 
 if __name__ == '__main__':
