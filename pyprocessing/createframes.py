44131c745a0dc9ed6b0bc295c4d1d26c37ad02c4
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2015-02-27 15:49:21 +0000

    changed read_data method to DataReader class, make it somewhat versatile

--- pyprocessing/createframes.py
+++ pyprocessing/createframes.py
@@ -8,22 +8,32 @@ import h5py
 import numpy as np
 
 
-def visit_all_objects(group, path, fileinfo=None):
+def visit_all_objects(group, path='', file_info=None):
 
-    if fileinfo is None:
-        fileinfo = {}
+    if file_info is None:
+        file_info = {}
+
+    for i in group.items():
+        if isinstance(i[1], h5py.Group):
+            visit_all_objects(i[1], path + '/' + i[0], file_info)
+        elif isinstance(i[1], h5py.Dataset):
+            dataset_name = path + '/' + i[0]
+            file_info[dataset_name] = (i[1].shape,i[1].dtype)
     
-    return fileinfo
+    return file_info
 
 
+class DataReader:
 
-def read_data(filename):
+    def __init__(self,filename):
+        try:
+            assert os.path.exists(filename)
+        except AssertionError as e:
+            e.args += (filename, 'does not exist')
+            raise
 
-    try:
-        assert os.path.exists(filename)
-    except AssertionError as e:
-        e.args += (filename, 'does not exist')
-        raise
+        if not h5py.is_hdf5(filename):
+            raise IOError('%s does not look like a hdf5 file' % filename)
 
-    if not h5py.is_hdf5(filename):
-        raise IOError('%s does not look like a hdf5 file' % filename)
+        self.file = h5py.File(filename,'r')
+        self.dset_info = visit_all_objects(self.file)
--- pyprocessing/test_createframes.py
+++ pyprocessing/test_createframes.py
@@ -16,16 +16,17 @@ class ReadDataTest(unittest.TestCase):
     def tearDown(self):
         pass
 
+    @patch('createframes.visit_all_objects')
+    @patch('createframes.h5py.File',return_value=True)
     @patch('createframes.h5py.is_hdf5', return_value=True)
     @patch('createframes.os.path.exists')
-    def test_given_filename_in_argument_when_method_invoked_then_check_file_existence(self, mock_exists, mock_ish5):
+    def test_given_filename_in_argument_when_method_invoked_then_check_file_existence(self, mock_exists, mock_ish5, mock_file, mock_visit):
 
         filename = 'some_file.hdf5'
 
-        createframes.read_data(filename)
+        createframes.DataReader(filename)
 
         mock_exists.assert_called_once_with(filename)
-
 
     @patch('createframes.os.path.exists',return_value=False)
     def test_given_filename_in_argument_when_method_invoked_then_assertion_error_if_file_nonexistent(self, mock_exists):
@@ -33,17 +34,23 @@ class ReadDataTest(unittest.TestCase):
         filename = 'some_file.hdf5'
 
         with self.assertRaises(AssertionError):
-            createframes.read_data(filename)
+            createframes.DataReader(filename)
 
-
-    def test_given_real_hdf5_file_when_class_invoked_then_hdf5_file_type_recognised(self):
+    def test_given_real_hdf5_file_when_called_then_hdf5_file_type_recognised(self):
 
         filename = '../p2m.hdf5'
 
         try:
-            createframes.read_data(filename)
+            createframes.DataReader(filename)
         except IOError:
-            self.fail('createframes.read_data IOerror raised')
+            self.fail('createframes.DataReader IOerror raised')
+
+    # def test_given_real_hdf5_file_when_called_then_return_dict_of_dicts_for_each_group(self):
+
+    #     with h5py.File('../p2m.hdf5') as h5_file:
+    #         data_dict = createframes.visit_all_objects(h5_file)
+
+    # def test_given_hdf5_file_when_called_then_return_dict_
 
 
 class VisitAllObjects(unittest.TestCase):
@@ -59,16 +66,27 @@ class VisitAllObjects(unittest.TestCase):
         with h5py.File('../p2m.hdf5','r') as h5_file:
             h5_group = h5_file[h5_file.keys()[0]]
 
-            data_dict = createframes.visit_all_objects(h5_group,'')
+            data_dict = createframes.visit_all_objects(h5_group)
 
             self.assertIsInstance(data_dict,dict)
 
     def test_given_non_hdf5_group_argument_when_called_then_return_empty_dict(self):
 
-        data_dict = createframes.visit_all_objects({},'')
+        data_dict = createframes.visit_all_objects({})
 
         self.assertIsInstance(data_dict,dict)
         self.assertTrue(len(data_dict)==0)
+
+    def test_given_hdf5_group_argument_when_called_then_return_dict_of_some_length(self):
+
+        with h5py.File('../p2m.hdf5','r') as h5_file:
+            h5_group = h5_file[h5_file.keys()[0]]
+
+            data_dict = createframes.visit_all_objects(h5_group)
+
+            self.assertEqual(len(data_dict),10)
+            self.assertIsInstance(data_dict.keys()[0],unicode)
+            self.assertTrue(data_dict.keys()[0].strip('/') in h5_group.keys())
 
 
 if __name__ == '__main__':
